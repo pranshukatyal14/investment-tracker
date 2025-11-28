ed058be505b6636a6b2a682fc3eb1f1b00b60c72
pranshukatyal14 <pranshukatyal62@gmail.com>
2025-11-29 01:39:56 +0530

    Update API URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { Investment, Category, MonthlyBudget } from "@/types/investment";
 
-const API_BASE = 'https://investment-tracker-7nx8.onrender.com';
+const API_BASE = 'https://investment-tracker-2-grqv.onrender.com/api';
 
 export const api = {
   // Investments
